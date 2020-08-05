f15ef954bf41d0d44701903b6e327f64a6f5eaa5
ShreedharYashoda <shreedhar022@gmail.com>
2020-08-05 19:48:34 -0400

    SpringBoot JPA H2

---
+++ src/main/java/com/example/demo/SpringBootJpaRestH2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaRestH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaRestH2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/model/Users.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USERS")
+public class Users {
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+	private Integer id;
+    @Column(name="name")
+	private String name;
+    @Column(name="teamname")
+	private String teamName;
+    @Column(name="salary")
+	private double salary;
+	
+	public Users() {}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
